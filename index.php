edec90b7cf74f0f31d287b8172272411df613da1
Daniel Espinosa <d111092.de@gmail.com>
2018-11-20 21:05:27 -0600

    prueba sakila payment

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 
 try {
     $mbd = new PDO('mysql:host=localhost;dbname=sakila', $usuario, $pass);
-    foreach($mbd->query('SELECT * from actor') as $fila) {
+    foreach($mbd->query('SELECT * from payment') as $fila) {
         print_r($fila);
+        print '<br>';
     }
     $mbd = null;
 } catch (PDOException $e) {
