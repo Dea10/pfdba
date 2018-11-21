082bd7585a44b8ca96e247e421ce44ceb6e2ca02
Daniel Espinosa <d111092.de@gmail.com>
2018-11-20 20:59:11 -0600

    prueba php-mysql sakila localhost

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
 
-  echo 'hola';
+  echo 'hola <br><br>';
   $usuario = 'root';
   $pass = '';
 
 try {
-    $mbd = new PDO('mysql:host=192.168.100.56;dbname=sakila', $usuario, $pass);
+    $mbd = new PDO('mysql:host=localhost;dbname=sakila', $usuario, $pass);
     foreach($mbd->query('SELECT * from actor') as $fila) {
         print_r($fila);
     }
