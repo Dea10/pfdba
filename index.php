18b50645d74c191026e0285c3f0b7ad611a94508
Daniel Espinosa <d111092.de@gmail.com>
2018-11-20 20:47:05 -0600

    prueba php-html

--- index.php
+++ index.php
@@ -28,22 +28,7 @@
 -->
     <?php 
       echo "hola php-html";
-   	  $usuario = 'root';
-      $pass = '';
-
-      try {
-        $mbd = new PDO('mysql:host=localhost;dbname=sakila', $usuario, $pass);
-        foreach($mbd->query('SELECT * from actor') as $fila) {
-          print_r($fila);
-        }
-        $mbd = null;
-      
-      } catch (PDOException $e) {
-          print "¡Error!: " . $e->getMessage() . "<br/>";
-          die();
-      }
-
-?>
+    ?>
 
 
 
