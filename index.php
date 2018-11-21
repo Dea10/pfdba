6959bf5f668f5e582b6a0e8f5a427223b1a2fa2a
Daniel Espinosa <d111092.de@gmail.com>
2018-11-20 20:57:06 -0600

    prueba php-mysql sakila

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
 
   echo 'hola';
+  $usuario = 'root';
+  $pass = '';
 
- ?>
+try {
+    $mbd = new PDO('mysql:host=localhost;dbname=sakila', $usuario, $pass);
+    foreach($mbd->query('SELECT * from actor') as $fila) {
+        print_r($fila);
+    }
+    $mbd = null;
+} catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
